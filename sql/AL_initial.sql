5953dfe426a4725b546fa88b0c3742200d4a7c38
Jason Dusterwald <wardust@gmail.com>
2016-12-18 22:56:07 -0500

    Continuing to flesh out DB tables; still have work to go.

--- sql/AL_initial.sql
+++ sql/AL_initial.sql
@@ -5,41 +5,8 @@
  * THIS IS UNTESTED, FIRST DRAFT STATUS!
  *
  * @author Jason Dusterwald
- * @last_updated 2016-12-17
+ * @last_updated 2016-12-18
  */
-
-/*
- * al_character - Individual characters
- */
-
-drop table if exists al_character;
-
-create table al_character(
-	id char(36) not null,
-	character_name_game varchar(24) not null,
-	character_name_last varchar(30),
-	character_name_first varchar(30),
-	character_name_middle_first varchar(30),
-	character_name_middle_second varchar(30),
-	id_character_class int not null,
-	id_character_faction int not null,
-	id_character_gender int not null,
-	character_level int not null,
-	id_character_race int not null,
-	primary key (id),
-	foreign key (id_character_class) references al_class(id) on delete cascade,
-	foreign key (id_character_faction) references al_faction(id) on delete cascade,
-	foreign key (id_character_gender) references al_gender(id) on delete cascade,
-	foreign key (id_character_race) references al_race(id) on delete cascade
-);
-
--- TODO: specialization(s), profession(s), and profession skill level(s)
-
-create trigger before_insert_al_character
-	before insert on al_character
-	for each row
-	set new.uuid = uuid()
-;
 
 /*
  * al_race - Races to which characters belong
@@ -114,11 +81,11 @@ create table al_specialization (
  
  drop table if exists al_specialization_role;
  
- create table al_specialization_role(
+create table al_specialization_role(
 	id int not null auto_increment,
 	name varchar(24) not null,
 	primary key (id)
- );
+);
 
 /*
  * al_specialization_type - Melee, ranged
@@ -126,13 +93,13 @@ create table al_specialization (
  
  drop table if exists al_specialization_type;
  
- create table al_specialization_type(
+create table al_specialization_type(
 	id int not null auto_increment,
 	name varchar(24) not null,
 	id_class int not null,
 	primary key(id),
 	foreign key (id_class) references al_class(id) on delete cascade
- );
+);
 
 /*
  * al_profession - Character professions/tradeskills
@@ -143,18 +110,36 @@ drop table if exists al_profession;
 create table al_profession (
 	id int not null auto_increment,
 	profession_name varchar(24) not null,
-	profession_type int not null,
-	profession_tier int not null,
-	primary key (id)
+	id_profession_type int not null,
+	id_profession_tier int not null,
+	primary key (id),
+	foreign key (id_profession_type) references al_profession_type(id) on delete cascade,
+	foreign key (id_profession_tier) references al_profession_tier(id)
 );
 
 /*
  * al_profession_type - Gathering, production
  */
  
+drop table if exists al_profession_type;
+ 
+create table al_profession_type (
+	id int not null auto_increment,
+	profession_type varchar(16) not null,
+	primary key (id)
+);
+ 
 /*
  * al_profession_tier - Primary, secondary
  */
+ 
+drop table if exists al_profession_tier;
+ 
+create table al_profession_tier (
+	id int not null auto_increment,
+	profession_tier varchar(16) not null,
+	primary key (id)
+);
 
 /*
  * al_player - Player/user
@@ -175,7 +160,40 @@ create table al_player (
 create trigger before_insert_al_player
 	before insert on al_player
 	for each row
-	set new.uuid = uuid()
+	set new.id = uuid()
+;
+
+/*
+ * al_character - Individual characters
+ */
+
+drop table if exists al_character;
+
+create table al_character(
+	id char(36) not null,
+	character_name_game varchar(24) not null,
+	character_name_last varchar(30),
+	character_name_first varchar(30),
+	character_name_middle_first varchar(30),
+	character_name_middle_second varchar(30),
+	id_character_class int not null,
+	id_character_faction int not null,
+	id_character_gender int not null,
+	character_level int not null,
+	id_character_race int not null,
+	id_player_owner char(36) not null,
+	primary key (id),
+	foreign key (id_character_class) references al_class(id) on delete cascade,
+	foreign key (id_character_faction) references al_faction(id) on delete cascade,
+	foreign key (id_character_gender) references al_gender(id) on delete cascade,
+	foreign key (id_character_race) references al_race(id) on delete cascade,
+	foreign key (id_player_owner) references al_player(id) on delete cascade
+);
+
+create trigger before_insert_al_character
+	before insert on al_character
+	for each row
+	set new.id = uuid()
 ;
 
 /*
@@ -191,4 +209,75 @@ create table al_race_faction (
 	primary key (id),
 	foreign key (id_race) references al_race(id) on delete cascade,
 	foreign key (id_faction) references al_faction(id) on delete cascade
+);
+
+/*
+ * al_race_class - Valid race/class combinations
+ */
+
+drop table if exists al_race_class;
+
+create table al_race_class (
+	id int not null auto_increment,
+	id_race int not null,
+	id_class int not null,
+	primary key (id).
+	foreign key (id_race) references al_race(id) on delete cascade,
+	foreign key (id_class) references al_class(id) on delete cascade
+);
+
+/*
+ * al_character_professions - Professions used by specific characters
+ */
+ 
+drop table if exists al_character_professions;
+
+create table al_character_professions (
+	id char(36) not null,
+	id_character char(36) not null,
+	id_profession int not null,
+	profession_skill_level int not null,
+	primary key(id),
+	foreign key (id_character) references al_character(id) on delete cascade,
+	foreign key (id_profession) references al_profession(id) on delete cascade
+);
+
+create trigger before_insert_al_character_professions (
+	before insert on al_character_professions
+	for each row
+	set new.id = uuid()
+);
+
+/*
+ * al_objective
+ */
+ 
+-- TODO: Finish this table, not fleshed out.
+ 
+drop table if exists al_objective;
+ 
+create table al_objective (
+	id char(36) not null,
+	id_objective_scope int not null,
+	primary key (id),
+	foreign key (id_objective_scope) references al_objective_scope(id) on delete cascade
+);
+ 
+create trigger before_insert_al_objective (
+	before insert on al_objective
+	for each row
+	set new.id = uuid()
+);
+ 
+/*
+ * al_objective_scope - Player or character - Is the player trying to get something done across
+ *	multiple characters, or is a specific character trying to accomplish something?
+ */
+
+drop table if exists al_objective_scope;
+
+create table al_objective_scope (
+	id int not null auto_increment,
+	objective_scope varchar(24) not null,
+	primary key (id)
 );
