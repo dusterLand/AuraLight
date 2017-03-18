681f0fc071a94d4eeaaae234c3739332200bbb85
Jason Dusterwald <wardust@gmail.com>
2017-03-17 22:09:30 -0400

    AL_Log seems to be working, if basic in its functionality.  Removed some unused commented out stuff.

--- application/lib/Common/Utility/AL_Log.php
+++ application/lib/Common/Utility/AL_Log.php
@@ -3,22 +3,45 @@
 namespace AuraLight\Common\Utility;
 
 class AL_Log {
-
+	
+	private $streamName;
 	/**
 	 * 
 	 */
-	public function __construct() {
+	public function __construct( $streamName ) {
+		$this->streamName = $streamName;
 	}
 	/**
-	 * 
+	 * TODO: Implement severity fence handling
 	 */
-	public function log() {
-	
+	public function log( $message, $severity ) {
+		$dateTime = date( "Y-m-d H:i:s" );
+		$logFile = fopen( '../var/logs/' . trim($this->streamName) . '.log', 'a' );
+		if( $logFile !== false ) {
+			$logMessage = '[' . $dateTime . '] ' . $message;
+			fwrite( $logFile, $logMessage . "\n" );
+			fclose( $logFile );
+		} else {
+			// TODO: Make some kind of exception handler to exit more gracefully
+			exit( __FILE__ . ': failed to obtain resource' );
+		}
 	}
 	/**
 	 *
 	 */
-	public function test1() {
-		return "test";
+	public function warn( $message ) {
+		$this->log( $message, 'warn' );
+	}
+	/**
+	 *
+	 */
+	public function info( $message ) {
+		$this->log( $message, 'info' );
+	}
+	/**
+	 *
+	 */
+	public function trace( $message ) {
+		$this->log( $message, 'trace' );
 	}
 }
--- application/lib/Controller/FrontPage/FrontPageController.php
+++ application/lib/Controller/FrontPage/FrontPageController.php
@@ -1,7 +1,5 @@
 <?php
 
-// include('log4php/Logger.php');
-
 namespace AuraLight\Controller\FrontPage;
 
 use AuraLight\Common\Utility\AL_Log;
@@ -14,24 +12,14 @@ class FrontPageController {
 	private $sql;
 	private $result;
 	private $log_frontpage;
-	private $testlog;
-	
-	
-		
 	/**
 	 * Constructor function.
 	 */
-	public function __construct( $config, $smarty, $conn ) {
+	public function __construct( $config, $smarty ) {
 		$this->conn = $conn;
 		$this->name1 = $config['app']['name1'];
 		$this->name2 = $config['app']['name2'];
-		$this->testlog = new AL_Log;
-//		Logger::configure( LOG_CONFIG . '/frontpage.xml');
-//		$this->log_frontpage = Logger::getLogger(__CLASS__);
-//		$this->log_frontpage->configure( LOG_CONFIG . '/frontpage.xml');
-//		Logger::configure(dirname(__FILE__) .'/../share/config/log4php/defaultLog4PHP.xml');
-//		$this->log_frontpage = $config['app']['log_frontpage'];
-//		$x = $config['log_config']['log_frontpage'];
+		$this->log_frontpage = new AL_Log( 'FrontPage' );
 		$smarty->template_dir =  '../lib/View/FrontPage/Template/';
 		$smarty->compile_dir = '../lib/View/FrontPage/Template_c/';
 		$smarty->config_dir = '../lib/View/FrontPage/Config/';
@@ -40,35 +28,28 @@ class FrontPageController {
 	 * Assign needed values.
 	 */
 	private function AssignValues( $smarty ) {
-//		$this->log_frontpage->trace('called');
+		$this->log_frontpage->trace('called');
 		$javascript = array(
 			'../../javascript/jquery/jquery-3.1.1.js',
 			'../../javascript/auralight.js',
 		);
-		//$smarty;
 		$smarty->assign( 'name1', $this->name1 );
 		$smarty->assign( 'name2', $this->name2 );
 		$smarty->assign( 'stylesheet', '/CSS/index.css' );
 		$smarty->assign( 'javascript', $javascript );
-		$smarty->assign( 'log', $this->testlog->test1());
 	}
 	/**
 	 * Render the page.
 	 */
-	public function DisplayPage($smarty) {
-		
+	public function DisplayPage( $smarty ) {
+		$this->log_frontpage->trace('called');
 		$this->sql = "SELECT id, race_name, race_reputation_name  FROM al_race";
 		$this->result = pg_exec($this->sql);
-
 		for ($lt = 0; $lt <pg_numrows($this->result); $lt++) {
 			$id = pg_result($this->result, $lt, 0);
 			$this->name1 = pg_result($this->result, $lt, 1);
 			$this->name2 = pg_result($this->result, $lt, 2);
 		}
-		
-		
-//		$this->log_frontpage->trace('called');
-		//global $smarty;
 		$this->AssignValues( $smarty );
 		$smarty->display( $smarty->template_dir[0] . 'index.smarty' );
 	}
